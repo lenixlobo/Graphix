acbf3f2675dbf192f860ef86b0d120b79eabb922
lenixlobo <lenixlobo@gmail.com>
2019-02-16 17:29:29 +0530

    multiple rotating boxes

--- main.cpp
+++ main.cpp
@@ -121,7 +121,20 @@ int main()
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 	
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f  ,  0.0f   , 0.0f),
+		glm::vec3(2.0f  ,  5.0f   , -15.0f),
+		glm::vec3(-1.5f ,  -2.2f  , -2.5f),
+		glm::vec3(-3.8f ,  -2.0f  , -12.3f),
+		glm::vec3(2.4f  ,  -0.4f  , -3.5f),
+		glm::vec3(-1.7f ,  3.0f   , -7.5f),
+		glm::vec3(1.3f  ,  -2.0f  , -2.5f),
+		glm::vec3(1.5f  ,   2.0f  ,  -2.5f),
+		glm::vec3(1.5f  ,   0.2f  ,  -1.5f),
+		glm::vec3(-1.3f ,   1.0f   , -1.5f),
 
+
+	};
 
 	
 	
@@ -198,8 +211,14 @@ int main()
 
 		glBindVertexArray(VAO);
 		//glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
-		glDrawArrays(GL_TRIANGLES,0,36);
-
+		for (unsigned int i = 0;i < 10;i++) {
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model,cubePositions[i]);
+			float angle = 20.0f*i;
+			model = glm::rotate(model, (float)glfwGetTime(),glm::vec3(1.0f,0.3f,0.5f));
+			ourshader.setMat4("model",model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
