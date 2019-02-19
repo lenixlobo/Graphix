362ad19604ccfc528dcf7e98986b38e997633387
lenixlobo <lenixlobo@gmail.com>
2019-02-19 18:34:46 +0530

    rotating camera

--- main.cpp
+++ main.cpp
@@ -192,11 +192,16 @@ int main()
 
 		//create 3d model
 		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 view;
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime())*radius;
+		float camZ = cos(glfwGetTime())*radius;
+
+		view = glm::lookAt(glm::vec3(camX,0,camZ),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f));
 		glm::mat4 projection = glm::mat4(1.0f);
 
 		model = glm::rotate(model , (float)glfwGetTime() , glm::vec3(0.5f,1.0f,0.0f));
-		view = glm::translate(view,glm::vec3(0.0f,0.0f,-3.0f));
+		//view = glm::translate(view,glm::vec3(0.0f,0.0f,-3.0f));
 		projection = glm::perspective(glm::radians(45.0f) , (float)scr_width/(float)scr_height , 0.1f,100.0f);
 
 		ourshader.use();
@@ -206,6 +211,9 @@ int main()
 		unsigned int viewLoc = glGetUniformLocation(ourshader.ID,"view");
 		//three ways to pass args
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		
+
 		glUniformMatrix4fv(viewLoc , 1, GL_FALSE, &view[0][0]);
 		ourshader.setMat4("projection",projection);
 
@@ -215,7 +223,7 @@ int main()
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model,cubePositions[i]);
 			float angle = 20.0f*i;
-			model = glm::rotate(model, (float)glfwGetTime(),glm::vec3(1.0f,0.3f,0.5f));
+			model = glm::rotate(model, glm::radians(angle),glm::vec3(1.0f,0.3f,0.5f));
 			ourshader.setMat4("model",model);
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
